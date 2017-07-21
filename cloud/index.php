b483d17f27e9a5d0c416e1f7f9e3988bf5691310
Lluís Bosch <lbosch@icra.cat>
2017-07-21 18:56:52 +0200

    canvi descripcio cloud

--- cloud/index.php
+++ cloud/index.php
@@ -1,1 +1,8 @@
-cloud
+<!doctype html><html><head>
+	<meta charset=utf-8>
+	<title>innoWatt->cloud</title>
+</head><body>
+
+<h1>innoWatt-&gt;cloud</h1>
+
+Descripció: plataforma integrada de gestió online: últim mòdul que serà desenvolupat
